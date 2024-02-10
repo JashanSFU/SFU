eccbe5bf5366b8ae5a4bb1ab4788479c9a080149
Jashanraj <jashanrajj@email.com>
2024-02-10 13:22:02 -0800

    removed temporary files

--- temp/PythonCode/Parameters/constants.py
+++
@@ -1,4 +0,0 @@
-DEVICE = "Analog Discovery 2"
-device_data=None
-
-## Add Frequency constant parameters to be used in each frequency set function (for consistency)
--- temp/PythonCode/help.py
+++
@@ -1,4 +0,0 @@
-import pkgutil
-
-for module in pkgutil.iter_modules():
-    print(module.name)
